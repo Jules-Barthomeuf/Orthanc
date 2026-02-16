184991a14f301e68a7a66751a1cae5bd84ad4cb4
Orthanc Dev <dev@orthanc.local>
2026-02-16 16:38:31 +0000

    Fix TS error + remove deprecated swcMinify

--- types/index.ts
+++ types/index.ts
@@ -17,6 +17,7 @@ export interface Property {
   ownershipHistory: OwnershipRecord[];
   marketData: MarketData;
   investmentAnalysis: InvestmentAnalysis;
+  annualOpex?: number;
 }
 
 export interface Document {
